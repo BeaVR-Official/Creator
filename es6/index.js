2fff7a8f648572187dbaaf9d7dbb97f9511a5f25
Pierre-Antoine Urvoy <pierreantoine.urvoy@gmail.com>
2016-04-24 21:28:22 +0200

    [Init] (All): Mise au propre
    Mise au propre et restructuration du projet à l'aide de la syntaxe ES6 et de webpack. Nettoyage des classes et séparation sous forme de logic/ui

---
+++ es6/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by urvoy_p on 24/04/16.
+ */
+
+import Creator from './Creator';
+
+new Creator("#mainView").display();
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './es6/index.js',
+    output: {
+        path: __dirname,
+        filename: 'creator.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'es6'),
+                query: {
+                    presets: 'es2015',
+                },
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+};
