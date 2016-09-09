149781749e0a8fa9c8cf18bd605094b982486b37
thomps-j <julien.e.thompson@gmail.com>
2016-09-09 20:41:00 +0200

    [Dev](VR) Orientation gyro Runner mobile
    Ajout des listeners pour la gestion de l'orientation de la
    camera pour le Runner sur mobile
    (fonctionnalite non terminee)

--- app/src/modules/runner/ScenePlayer.ui.js
+++ app/src/modules/runner/ScenePlayer.ui.js
@@ -1,14 +1,41 @@
 import ScenePlayer from './ScenePlayer';
+import Constants from '../creator/Constants';
 
 class ScenePlayerUI {
 
   constructor() {
+    this.adaptToWindow();
     $('#mainView')
       .click(() => {
         //console.log(e);
         ScenePlayer.load();
       });
+
+    $('#mainView').append(ScenePlayer._renderer.domElement);
   }
+
+  /*
+  constructor() {
+    this.adaptToWindow();
+    //$(window).resize(() => this.adaptToWindow());
+    //$('#mainView').append(Scene._renderer.domElement);
+    //SceneControls.constructor();
+  }
+  */
+
+  adaptToWindow() {
+    // let parentWidth  = $(window).width() - ($('.categories-left-panel').width() +
+    // $('.properties-left-panel').outerWidth()); let parentHeight = $(window).height();
+
+    let parentWidth  = $(window).width() - Constants.getCanvasSettings().width;
+    let parentHeight = $(window).height();
+
+    ScenePlayer._camera.aspect = parentWidth / parentHeight;
+    ScenePlayer._camera.updateProjectionMatrix();
+    ScenePlayer._renderer.setSize(parentWidth, parentHeight);
+    ScenePlayer.render();
+  }
+
 }
 
 export default new ScenePlayerUI();
