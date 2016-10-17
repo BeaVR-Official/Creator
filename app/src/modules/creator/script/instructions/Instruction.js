8df1426a844150f515c420c361a495c56a25bd05
thomps-j <julien.thompson@epitech.eu>
2016-10-17 13:24:25 +0200

    [Dev](Script) Correction génération UUID
    Correction de la génération de l'UUID pour les
    objets-modèles Condition et Instruction

--- app/src/modules/creator/script/instructions/Instruction.js
+++ app/src/modules/creator/script/instructions/Instruction.js
@@ -9,7 +9,7 @@
 
 export default class Instruction {
   constructor() {
-    this.uuid   = guid;
+    this.uuid   = generateUUID();
     this.type = 0;
     this.data = null;
   }
@@ -24,3 +24,13 @@ export default class Instruction {
     return (true);
   }
 }
+
+function generateUUID() {
+  let d = new Date().getTime();
+  let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+};
