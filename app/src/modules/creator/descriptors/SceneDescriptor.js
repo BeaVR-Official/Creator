0179b911bd9c7f033c31c0f0f2d69c48455ed3be
thomps-j <julien.e.thompson@gmail.com>
2016-10-25 22:11:24 +0200

    [Dev](Architecture) SceneDescriptor mise a jour
    Mise à jour de la classe SceneDescriptor

--- app/src/modules/creator/descriptors/SceneDescriptor.js
+++ app/src/modules/creator/descriptors/SceneDescriptor.js
@@ -1,5 +1,50 @@
+import UUID from './../../utils/UUID';
+import ObjectDescriptor from "ObjectDescriptor";
+
 export default class SceneDescriptor {
-  constructor() {
-    this.uuid = generateUUID();
+  constructor(name) {
+    this.uuid              = UUID.createUUID();
+    this.name              = name;
+    this.objectDescriptors = [];
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return (this.name);
+  }
+
+  setObjectDescriptors(objectDescriptors) {
+    this.objectDescriptors = objectDescriptors;
+  }
+
+  getObjectDescriptors() {
+    return (this.objectDescriptors);
+  }
+
+  getObjectDescriptorIndex(objectUuid) {
+    for (let index = 0; index < this.objectDescriptors.length; index++) {
+      if (this.objectDescriptors[index].uuid === objectUuid) {
+        return (index);
+      }
+    }
+    return (-1);
+  }
+
+  addObjectDescriptor(name, type) {
+    let newObjectDescriptor = new ObjectDescriptor(name, type);
+    this.objectDescriptors.push(newObjectDescriptor);
+    return (newObjectDescriptor.uuid);
+  }
+
+  removeObjectDescriptor(objectUuid) {
+    let index = this.getObjectDescriptorIndex(objectUuid);
+    if (index === -1) {
+      return (false);
+    }
+    this.objectDescriptors.splice(index, 1);
+    return (true);
   }
 }
