6ffab9c733e1919109a58a1cac5732b6fb6589cf
thomps-j <julien.e.thompson@gmail.com>
2016-10-25 22:28:48 +0200

    [Dev](Architecture) ObjectDescriptor mise à jour
    Mise à jour de la classe ObjectDescriptor

--- app/src/modules/creator/descriptors/ObjectDescriptor.js
+++ app/src/modules/creator/descriptors/ObjectDescriptor.js
@@ -1,7 +1,64 @@
 import UUID from './../../utils/UUID';
 
 export default class ObjectDescriptor {
-  constructor() {
+  constructor(name, type) {
     this.uuid = UUID.createUUID();
+    this.name = name;
+    this.type = type;
+    this.position = [0.0, 0.0, 0.0];
+    this.rotation = [0.0, 0.0, 0.0];
+    this.scale = [0.0, 0.0, 0.0];
+    this.solidState = false;
+    this.gravityEffected = false;
+    // TODO: support all the more specific properties
+    // namely: Cameras, Skyboxes
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return (this.name);
+  }
+
+  setPosition(position) {
+    this.position = position;
+  }
+
+  getPosition() {
+    return (this.position);
+  }
+
+  setRotation(rotation) {
+    this.rotation = rotation;
+  }
+
+  getRotation() {
+    return (this.rotation);
+  }
+
+  setScale(scale) {
+    this.scale = scale;
+  }
+
+  getScale() {
+    return (this.scale);
+  }
+
+  setSolidStatus(isSolid) {
+    this.scale = isSolid;
+  }
+
+  getSolidStatus() {
+    return (this.solidState);
+  }
+
+  setGravityStatus(isGravityEffected) {
+    this.gravityEffected = isGravityEffected;
+  }
+
+  getGravityStatus() {
+    return (this.gravityEffected);
   }
 }
